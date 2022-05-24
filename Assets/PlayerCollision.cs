a982ecf9704e9b63250ad823d2004a57d1acb40b
LuchoDelArco <47138072@est.ort.edu.ar>
2022-05-24 15:59:43 -0300

    Initial commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter(Collision col)
+	{
+	
+		if (col.gameObject.name == "DeathWall")
+		{
+			transform.position = new Vector3(-2, 0.5f, 0);
+		}
+
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.name == "Plane")
+		{
+			transform.position = new Vector3(-2, 0.5f, 0);
+		}
+	}
+}
